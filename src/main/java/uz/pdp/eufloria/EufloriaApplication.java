f8c509628ab16175fecd752a6f888b3fdb6cae0d
user <odilxonabdullayev9@gmail.com>
2024-02-27 20:57:30 +0500

    CardController test

--- src/main/java/uz/pdp/eufloria/EufloriaApplication.java
+++ src/main/java/uz/pdp/eufloria/EufloriaApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @SpringBootApplication
---
+++ src/main/java/uz/pdp/eufloria/dto/card/CardCreateDto.java
@@ -0,0 +1,14 @@
+package uz.pdp.eufloria.dto.card;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CardCreateDto extends CardBaseDto {
+    private String type;
+}
