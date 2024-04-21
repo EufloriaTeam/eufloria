d3dacf1f73e904904c81e5be432871cb69c62448
RakhimDeSanta <santaclousx1@gmail.com>
2024-04-21 12:52:26 +0500

    Google Cloud credentials fixed

--- src/main/java/uz/pdp/eufloria/common/AppConstants.java
+++ src/main/java/uz/pdp/eufloria/common/AppConstants.java
@@ -4,5 +4,5 @@ public interface AppConstants {
     String TOKEN_TYPE = "Bearer ";
     String AUTH_HEADER_NAME = "Authorization";
     String BASE_PATH = "/api/v1";
-    String BUCKET_NAME = "eufloria-bucket";
+    String BUCKET_NAME = "eufloria-buck";
 }
